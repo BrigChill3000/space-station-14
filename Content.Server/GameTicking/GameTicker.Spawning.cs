78e7b2913c2dd1d4b74a559b99b962b898788bb6
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-09-07 14:57:37 +0300

    Update GameTicker.Spawning.cs

--- Content.Server/GameTicking/GameTicker.Spawning.cs
+++ Content.Server/GameTicking/GameTicker.Spawning.cs
@@ -241,7 +241,7 @@ namespace Content.Server.GameTicking
                 _chatSystem.DispatchStationAnnouncement(station,
                     Loc.GetString("latejoin-arrival-announcement",
                         ("character", MetaData(mob).EntityName),
-                        ("gender", character.Gender), // Corvax-LastnameGender
+                        ("gender", character.Gender), // RUS-Edit
                         ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                     Loc.GetString("latejoin-arrival-sender"),
                     playDefaultSound: false);
